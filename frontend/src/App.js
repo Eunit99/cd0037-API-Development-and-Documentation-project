397c2c7e54b6d175a0f13e7a9ae4fc9f3be30f93
Emmanuel Uchenna <eunithelp@gmail.com>
2022-06-23 10:55:36 +0100

    Refactor: Refactor code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,21 @@ import Header from './components/Header';
 import QuizView from './components/QuizView';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Header path />
-        <Router>
-          <Switch>
-            <Route path='/' exact component={QuestionView} />
-            <Route path='/add' component={FormView} />
-            <Route path='/play' component={QuizView} />
-            <Route component={QuestionView} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <Header path />
+                <Router>
+                    <Switch>
+                        <Route path='/' exact component={QuestionView} />
+                        <Route path='/add' component={FormView} />
+                        <Route path='/play' component={QuizView} />
+                        <Route component={QuestionView} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
